ed9196feb5511feb167f6d39f7a24505fda6ee56
Dima <darkness1198@gmail.com>
2021-06-11 16:29:31 +0300

    Added: swift package: "Swift Alerts", alert with countries

--- NewsApp/Home Page/Model/DataFetcherService.swift
+++ NewsApp/Home Page/Model/DataFetcherService.swift
@@ -17,17 +17,17 @@ class DataFetcherService {
     }
     
     func fetchNews(selectedCountry: Int, selectedCategory: Int, completion: @escaping (NewsModel?) -> Void) {
-        let urlNewsString = "https://newsapi.org/v2/top-headlines?country=\(K.countriesIndex[selectedCountry])&category=\(categories[selectedCategory])&apiKey=\(K.apiKey)"
+        let urlNewsString = "https://newsapi.org/v2/top-headlines?country=\(K.countriesIndex[selectedCountry])&category=\(categories[selectedCategory])&apiKey=\(apiKey)"
         networkDataFetcher.fetchGenericJSONData(urlString: urlNewsString, response: completion)
     }
     
     func fetchNewsWithCategory(selectedCountry: Int, selectedCategory: Int, completion: @escaping (NewsModel?) -> Void) {
-        let urlNewsWithCategory = "https://newsapi.org/v2/top-headlines?country=\(K.countriesIndex[selectedCountry])&category=\(categories[selectedCategory])&apiKey=\(K.apiKey)"
+        let urlNewsWithCategory = "https://newsapi.org/v2/top-headlines?country=\(K.countriesIndex[selectedCountry])&category=\(categories[selectedCategory])&apiKey=\(apiKey)"
         networkDataFetcher.fetchGenericJSONData(urlString: urlNewsWithCategory, response: completion)
     }
     
     func fetchNewsWithCountry(selectedCountry: Int, selectedCategory: Int, completion: @escaping (NewsModel?) -> Void) {
-        let urlNewsWithCategory = "https://newsapi.org/v2/top-headlines?country=\(K.countriesIndex[selectedCountry])&category=\(categories[selectedCategory])&apiKey=\(K.apiKey)"
+        let urlNewsWithCategory = "https://newsapi.org/v2/top-headlines?country=\(K.countriesIndex[selectedCountry])&category=\(categories[selectedCategory])&apiKey=\(apiKey)"
         networkDataFetcher.fetchGenericJSONData(urlString: urlNewsWithCategory, response: completion)
     }
 }
